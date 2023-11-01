858b3ddedec211d5e1d6de53481cc1ce63b0089c
Thanh Bui <vtbui@linagora.com>
2023-11-01 21:43:37 +0700

    fixup! [HW4] Multi-thread TCP server

--- Homework4/Ex2/src/courses/courses.h
+++ Homework4/Ex2/src/courses/courses.h
@@ -27,8 +27,8 @@ void displayCourses(struct CourseList *courseList);
 void getStudentRegistrationList(struct RegistrationList *registrationList, char studentID[], struct CourseList* courseListByStudentID);
 void getCourseListByCourseIDAndWeekDay (struct CourseList *courseList, struct CourseList *courseListByStudentID, char weekday[]);
 void displayCourseList(struct CourseList *courseList);
-void displayCourseListByStudentIDAndWeekDay(struct RegistrationList *registrationList, struct CourseList *courseList, char studentID[], char weekday[]);
-void displayALLCoursesByStudentID(struct RegistrationList *registrationList, struct CourseList *courseList, char studentID[]);
+void displayCourseListByStudentIDAndWeekDay(struct RegistrationList *registrationList, struct CourseList *courseList, char studentID[], char weekday[], int connfd);
+void displayALLCoursesByStudentID(struct RegistrationList *registrationList, struct CourseList *courseList, char studentID[], int connfd);
 void freeCourseList(struct CourseList *courseList);
 
 #endif
